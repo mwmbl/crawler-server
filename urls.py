200d80be1696ec2f4e5669ca253949e4ac36cd93
Daoud Clarke <daoud.clarke@gmail.com>
2022-05-30 22:16:41 +0100

    Add code to create URL table in Postgresql

--- urls.py
+++ urls.py
@@ -2,24 +2,63 @@
 Database storing info on URLs
 """
 import os
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
 
 from psycopg2 import connect
+from psycopg2.extras import execute_values
+
+
+class URLState(Enum):
+    """
+    URL state update is idempotent and can only progress forwards.
+    """
+    NEW = 0         # One user has identified this URL
+    CONFIRMED = 1   # A different user has identified the same URL
+    ASSIGNED = 2    # The crawler has given the URL to a user to crawl
+    CRAWLED = 3     # At least one user has crawled the URL
+
+
+@dataclass
+class URLStatus:
+    url: str
+    state: URLState
+    user_id_hash: str
+    updated: datetime
 
 
 def get_connection():
-    return connect(os.environ['DB_URL'])
+    return connect(user=os.environ["USERNAME"])
 
 
-def create_tables():
+def create_tables(conn):
     sql = """
     CREATE TABLE IF NOT EXISTS urls (
-        url STRING PRIMARY KEY,
-        status INT NOT NULL,
+        url VARCHAR PRIMARY KEY,
+        status INT NOT NULL DEFAULT 0,
+        user_id_hash VARCHAR NOT NULL,
         updated TIMESTAMP NOT NULL DEFAULT NOW()
     )
     """
 
-    conn = get_connection()
-    conn.execute(sql)
+    with conn.cursor() as cursor:
+        cursor.execute(sql)
 
 
+def update_url_status(conn, url_statuses: list[URLStatus]):
+    sql = """
+    INSERT INTO urls (url, status, user_id_hash) values %s
+    ON CONFLICT (url) DO UPDATE SET status=excluded.status, user_id_hash=excluded.user_id_hash
+    """
+
+    data = [(status.url, status.state.value, status.user_id_hash) for status in url_statuses]
+
+    with conn.cursor() as cursor:
+        execute_values(cursor, sql, data)
+
+
+if __name__ == "__main__":
+    with get_connection() as conn:
+        create_tables(conn)
+        update_url_status(conn, [URLStatus("https://mwmbl.org", URLState.NEW, "test-user", datetime.now())])
